6463b1eb16c26d798212fb186922e50fe8d40bff
Noyon-Tech-Solution <shahadathosen8987@gmail.com>
2024-10-05 20:25:44 +0600

    Noyon Tech Solution

---
+++ React-Project/src/Component/Maping Data.jsx
@@ -0,0 +1,11 @@
+function Maping (props){
+    const {titleText, descText} = props;
+    return(
+        <div className="card">
+            <h1 className="heading2">{titleText}</h1>
+            <p className="card">{descText}</p>
+            <p className="date">05/10/2024</p>
+        </div>
+    )
+}
+export default Maping;
