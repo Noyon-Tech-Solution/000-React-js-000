0c6b3a6bc370e71d501f62e2e9c228555f4910f5
Noyon-Tech-Solution <shahadathosen8987@gmail.com>
2024-10-16 19:25:56 +0600

    Noyon Tech Solution

--- My-React-App/src/App2.jsx
+++ My-React-App/src/App2.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from "react";
 // import Hooks_useState from './Component2/Hooks_useState/Hooks_useState'
 // import Update_useState from './Component2/Hooks_Update_useState/Udate_useS
 // import Event_Bubbling from './Component2/Event_Bubbling/Event_Bubbling'
 // import From from './Component2/Form/From'
+// import Child from "./Component2/State_Lefting/Child";
+
+// 05 How to Send Data Parents to Child React.Js
+// const data = "I am from Parents";
 
 export default function App2() {
+  // 05 How to Send Data Child to Parents React.Js
+  // const handlerChildData = (e) => {
+  //   console.log(e);
+  // };
+
   return (
     <div>
       {/*  01 How to use Hooks_useState in React.Js */}
@@ -18,6 +27,11 @@ export default function App2() {
 
       {/* 04 How to Access of From Value in React.Js */}
       {/* <From /> */}
+
+      {/* 05 How to Send Data Child To Parents  & Parents to Child React.Js */}
+      {/* <Child data={data} onChildData={handlerChildData} /> */}
+
+      
     </div>
-  )
+  );
 }
--- My-React-App/src/Component2/Form/From.jsx
+++ My-React-App/src/Component2/Form/From.jsx
@@ -1,29 +1,51 @@
 import React, { useState } from "react";
 
 export default function From() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  //! How To Use UseState with Object in React Js..........
+  //? How To Use UseState with Object in React Js..........
 
-  const handlerNameSubmite = (e) => {
-    setName(e.target.value);
+  //! This Code Structure is not Good Practice:
+  // const [name, setName] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+
+  // const handlerNameSubmite = (e) => {
+  //   setName(e.target.value);
+  // };
+  // const handlerEmailSubmite = (e) => {
+  //   setEmail(e.target.value);
+  // };
+  // const handlerPasswordSubmite = (e) => {
+  //   setPassword(e.target.value);
+  // };
+  // const handlerSubmite = (e) => {
+  //   console.log("From is Submited");
+  //   const userInfo = {
+  //     name,
+  //     email,
+  //     password,
+  //   };
+  //   console.log(userInfo);
+  //   e.preventDefault();
+  // };
+  //! This Code Structure is not Good Practice:
+
+  //? How To Use UseState with Object in React Js..........
+  //! How To Use UseState with Object in React Js..........
+
+  //? This Code Structure is Good Practice:
+  const [user, setUser] = useState({ name: "", email: "", password: "" });
+  const { name, email, password } = user;
+
+  const handlerChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
   };
-  const handlerEmailSubmite = (e) => {
-    setEmail(e.target.value);
-  };
-  const handlerPasswordSubmite = (e) => {
-    setPassword(e.target.value);
-  };
+
   const handlerSubmite = (e) => {
-    console.log("From is Submited");
-    const userInfo = {
-      name,
-      email,
-      password,
-    };
-    console.log(userInfo);
+    console.log(user);
     e.preventDefault();
   };
+  //? This Code Structure is Good Practice:
 
   return (
     <div>
@@ -35,9 +57,11 @@ export default function From() {
           type="text"
           name="name"
           id="name"
+          placeholder="Enter Your Name"
           value={name}
           required
-          onChange={handlerNameSubmite}
+          onChange={handlerChange}
+          //! onChange={handlerNameSubmite}
         />
         <br />
         <label htmlFor="email">Email:</label>
@@ -46,23 +70,27 @@ export default function From() {
           type="text"
           name="email"
           id="email"
+          placeholder="Enter Your Email"
           value={email}
           required
-          onChange={handlerEmailSubmite}
+          onChange={handlerChange}
+          //! onChange={handlerEmailSubmite}
         />
         <br />
         <label htmlFor="password">Password:</label>
         <input
           style={{ margin: "10px" }}
-          type="number"
+          type="text"
           name="password"
           id="password"
+          placeholder="Enter Your Password"
           value={password}
           required
-          onChange={handlerPasswordSubmite}
+          onChange={handlerChange}
+          //! onChange={handlerPasswordSubmite}
         />
         <br />
-        <button type="submit">Register</button>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
